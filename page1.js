94fcaf452a967ffc9c32987923733f3374f5b2ce
Evossss <118131736+Evossss@users.noreply.github.com>
2022-11-13 20:35:27 +0100

    Update page1.js

--- page1.js
+++ page1.js
@@ -3,7 +3,7 @@ var boutonValider = document.getElementById("boutonValider");
 
 boutonValider.onclick= function() {
      if (reponse.value === "F.1.4"){
-        reponse.value="";  //lien de la page suivante 
+        reponse.value="https://evossss.github.io/2/";  
     }
     else{reponse.value="Falsche Antwort";
 }
